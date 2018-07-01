7ab756ae842297b201f3d42fed5b25bb98ce7836
lrylander <linnearylander@gmail.com>
2018-07-01 14:29:39 -0500

    update image links

--- src/views/APEcon/APEcon.js
+++ src/views/APEcon/APEcon.js
@@ -13,7 +13,7 @@ class Cards extends Component {
                 Microeconomics Review Guide
               </CardHeader>
               <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/7GY9415HjvUT6PCRY0bXyPJ4U0Wwv2s5BMr5ybCvEIB9kv70qQh-1BDZikjhbXQ1BYoUBGkNdgXh_ZfjwKbrEYV74dqRkfRDQ464LsAkZRDeTLB2D7tHU276jM9eD4SPP4_50oyhqm3i_wVQdz6x36l8p1rCinDldUFEq6bR_8bCcEBx_wEZQSxMbsSSQiqKUtQY1ss2UCsHtLULUtNg4tG2ndp8JwIZb58pR3pf49G81xboKWA2aKhAdb3YMBzE91SEk9Go6Q_BhKMLH-d1g6FJmNwly-7wQt0Tg_h3fK0bfBcOkLbTseNKrSqjTmf_H7yr0wCpSv7XYjPocXjFZx6qPIarS-EU2DWN7Dqxa290jgIs82tO3XE5-Z7MJibCy0vm-I_5Gznb7YpSXIwy8ds6KI68ByfLuAbZ5NnaPr557i-LQsyYFGyrHiIsjR-UiCb_DjvChBiuOmXAdPi5DfLZo83Ym2shribMJwwfJOgjO4bjK21gpmDgYbru4981yXz82H2GiBRFiYJaRvk2qfK3pGzIxjQOk37HBSVJM0p17vHHguyID4XZlenRUi4j9ZQpD98wSLxd_rYGPaKYUM7H61cJwR3Xfi8oDNo=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/MUYlKIeiUAThhBblHaELSj5UdrktCMstAl9cXflIIETDBxx1iBvil0UsQJhi4PQmOEVupBgpItJ1OMLDs8N1A7sUXz2hu2sBJh_2sRmcqj2wl2L6jW7wJsfGSSWV1Hw9PGEUtgKilA=w2400"/>
               </CardBlock>
             </Card>
           </a>
