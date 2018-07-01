57f9678ff6307373fcb5d54b6623df6a81545d98
lrylander <linnearylander@gmail.com>
2018-07-01 15:09:08 -0500

    Calc 3 & AP Physics C images

--- src/views/APPhysicsC/APPhysicsC.js
+++ src/views/APPhysicsC/APPhysicsC.js
@@ -13,7 +13,7 @@ class Cards extends Component {
                 Mechanics
               </CardHeader>
               <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/9LM8oj4XV7_FfTMrLvKS3ooz4RI7GOOTp0AM-RC0MzAyYFqW1KiKYVRB9nIxOqqemaqB9ZA8iiBmTujVicAv0j0foi08pVc8jPedl7wZ94f4frOalkqTzTmIG9Pwm_BmBHJvb15pg_j8ZWLyUiuxWGTGG4ETWIBn3RoOO8ESmPCktLwp_VFC_New4NIhUl_jRdSu7sV9mW1BoMNuyp3UCES5-Fqp-KFxGZHQmNgJknbpUZNyIlkBNIdpuaA67lVwLqxxLtXfAhrLmGv-RwYSbkVKhrb-9mQnB0rQMYiVzxdE8d4E5g6UotUJ_5fHdfKPVI98j-BZmnyEXB8WvY54Z7DshBjJzN4m6iisFVsXHWuAyIIZSvGN5QTYWkzlkn887zxTrNIC-R3MURXCf_MovExhbF8sOZug7UdSL8l-jhSPdvAfxyW_3zuBt2ELOlQjmgPqNgF_CrPcdtEMjCm7G1Ig1a361vdHsWgwvjD9EE4kSXPXOKxRZl-dGKBJxZ-np56q51FYK7psWszBirk8CJpCJxVFJ8aDqFMAFaQ_mu3jd2ItAZZ0sOKHrQXgDHZ7uOrOQS8oOoD6NjsgEc2bVh__1ulm_RInGaZ_aFk=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/oLAg7P7SNyrin07dkVvIqa2kjWQOx0I7UmEFOeSkc5DQ28TwIZHYeu27ahVUduC7_LsdEenmrmnKiJ8x3fwfAI6fqpjyOr4BOmKAoMeMcBgQJt2vzCMWa0URgAQrjXMyLgyi5gSeyA=w2400"/>
               </CardBlock>
             </Card>
           </a>
@@ -25,7 +25,7 @@ class Cards extends Component {
                 Electricity & Magnetism
               </CardHeader>
              <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/Nsm6a1SFtT3xLJExRpzx6Pm3dBNi6qA1jPLuGrqcAa18I4b-zTW5hgmJapIxKcatLOHcVPpndLLZMYMRH4NCEZIIUpO7P2anuv0QVEwAmTGtiKG-0Ui1eSHrbLB8uaq3hYQp0uXXyksvweTLkxsikOb0G197wb02EY3FFZLhlRyG85L2tWEwnbmJYn64BupkUi79DKaJIbooCZ5Ju2Oed0AVg_eFKyoTz2agqnsubUOH-fZ-DyKhPvIse2SbV0NAm6eBwN6lElKytnCnBqY2cuLEK681rq1aLeBUprBxquSqFO0ZX4g4YT3SthGWdcu1lGAm8UizXBsE9VuZeBS31rXpHtsWrysTxiEFLYmieczwggMCn4Ma9FZgNTujDTYQhkpy2O3m8ouAWVVXu4r7cArcbRHhFf3zGmIEkizr05sU4N3r47EQJcHSBeuwgxYfIK5Ddh__w8-7DjOEeSocpjSjq-SZmsKCLekDCYuFa6k7ulx55OxhzBIdgVzv-WhUW5VZmBnIF-4lC65w4PWkbBLsUO9Q8-Lhahi4-zwGbRZRucPNCQnEy-NyXVGpHkbup0xbxWc99zjjnZikQNRm5-ROzt3RBiyqGzQwWRE=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/ttDMOAk0itLsGDDv3vBeVTSSQBnTQvncUyEL6jqekpqglhS-Sc_oU5SjI-9Gsv5QyQd24tJvYNGYERRoZU6-T-PcADOVRHay0wYzFx0Ca2lvZs2bH7_dlMNBGGQ40MDjlmErkuJcog=w2400"/>
               </CardBlock>
             </Card>
           </a>
--- src/views/Calc3/Calc3.js
+++ src/views/Calc3/Calc3.js
@@ -13,7 +13,7 @@ class Cards extends Component {
                 Chapter 11
               </CardHeader>
               <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/yucSun-rzyrygEHfRnSUrrr6sM5swl3DbJACceLR26-7oNbBEgSOj_23061kyKzbTZr-xbQvhAX_kc6HX5Y8de1GIv07ztMARncstfplWIV4tFY8CRvualXCQ1Xl583DshKCFDc4sIZdQ7LnBTbzGYch6KnsQtwgIuI9SOZzodr8Ae26eOW0-CZA1Eh1whTVrm5DhdyJptgoyoY-QuuOGOj5OxdtTy_A1_uwuCTiWpSHCTNL9WB6TWu-MdiSnAp9SzqeSPlTxUbnAxkFZkXjL5yAL7VHckTrvBH7x6pgvkSnE7Wbe7E3wEOoAB8BuqgMPD7kSvP9qe7s9sLn-rvIwA90MxGoaOIW2b-7je30_RSoVs95834CLWMZ1BInvHVqyq-IXWHPlsCs2hKqUCCKNVDF10WJdNUa_oUzoLwO_zNPsDyJ7qwUMOCiHEMe9Z3vWB73mzHQ1r1Ehi-faDig34gAoA7ow7WqnMS67dCxv8E3m3Je9wkqwnMhkrCYcnyG4dRJY3jpQj7p9QJBP2-lEUyGqXXC8YfEc1fPM-yej0Z8C6oK-qzE7-noFMRSZ5PqTj4EJDR8c7iv4zzblwgHuIwu5Amu0JIdTwdA8-E=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/OPNBF0uH9fGN6d2E-5IznTz2fdpKnPASVFLi_lxYKM391i_wb71HZRWycJiwKBWse-yzFRDxf4MwwWJPYRPxcjdu20v_q8Kxx-emWvMVklu4grCO5DoLJcvxiKj9dUNKEs_gJryeSw=w2400"/>
               </CardBlock>
             </Card>
           </a>
@@ -25,7 +25,7 @@ class Cards extends Component {
                 Chapter 12
               </CardHeader>
              <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/SO13JYfwJKjXHZU18_sltcQ3CXgeUXehscob7j2_ArWr_EpWxLAm9wD6wO2kpw9PmUcO6oXET74AK3RYOSPIlLUDMJP9ydRSx70duIznNj8idFIbi4R3JAqXOLSI-mGUjzPIVsjCcZ5JWZzJg7TkIDAGZVXLKXQIQkyYOokNOUw8vxi_qupOY0mlzET9p_zEbuER_B8mEKREAkCWWx05kPRjctGmy6vNsaaYncjcEoXySbVQajbPN-j4oDk3pyHpklmBEkZsvMRd-XOUrrclPk2v9dVzKHSCo6hy7w_fkyXMeBSM2Vh1C15B_Qbkis8vn4d-JFbm3CMAP5RzCohAAKLeGhVW6BAOPvBokW2-xPCAg5J551sKGHhkW7xDcqvZLW0EsG2vHsmb4yR5_dhxdenAjakLtOC3mdbnN7ifKI1mWpZCz9-eMeLnss3qG1GSIvW3U1-wz3tJeZs0oOYY4ItU_2WNlSOvkDIyfxBgLXn4LeD9N_L1Bwu1E0_1JHq5blt39YpTkAyx2s9NyBkLAbMvc7IuT4GSMb6kCs6mPU1mekRCdCaP_786rMMp7slQCTwaCRckUKYYI1NOBQilYjB3Mukz-r70ctWu_k0=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/Mw8ylDbY2goe0QnHxyfpD202NCqJoUEGdv39MMJmqwDSDUyv4fUkc9z9HvJ71HQjk34CExWSey6FFjBcSOboclGMYTQ6wpmaFO-JWXdBDcgRQguLSPgOYa-VAfYy0aJUZT7psicLvw=w2400"/>
               </CardBlock>
             </Card>
           </a>
@@ -37,7 +37,7 @@ class Cards extends Component {
                 Chapter 13
               </CardHeader>
               <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/e8oY5Z4Qjn_5zQt1hJpujFhbqG2YMlMM9q0f1mpHiMJgCwDF1zctXSh8kPMq9cPYO6amX0KByeL6C3DYpm8eqJHdygz0fLF6zfCaWC9F8xyJpJbTrH5jUD8VocaNnNMNuSWGwy1WZqflyJczQTDtrAzfUhWabfk6kozor_6rgTjFS79Vf4AfSliqc8IAsMasVfBNNQeKVRLjdPfV5vj4HmDXFPmpahMu-QVFtjooyZqr7w5cnn3DkLW0cA9lH-dLrA25ou1qdFSIDkXKKGQh64CO8YYDbb0a7YdHujIBqiCfEoXjhQI4Zjh9-q86AlduKmdly30xr1T9lFucncjiaNqcbY1UD9D_jAz3JLXetO8Nv1A9UGMwWO99EQToUB14t68RouIbe8ouKs0j65NPuFuD_VtYUiDspx3C2wulU0bdFZNMPPGzSIR_66oWB_zeG1MIKY5IYGaH2rBw3yzQsAQC9Fzb9_n_-rctxbptpTgp3vU3WC0WOc7QxHqGfuzdz-YuDI2WUQyeQM7xXeE6fN8XMtLsCni6fTHdTrcMTS0ov_8pj7ASq6R2SpoPVeZYGElIqDEHrShAKw02lymIqNFnw17BRtzIgmPjihA=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/yWvQnpDoDMHcHBdob7ptbU_vWc2WOnB3Q5KuHYOoI8gBiX_2uxFLCweAooXyeoF8ZCU9Onq8EQkSb18xIArRn_wDvryxX31g8_3pdft4YImO-JGbcvNOBsAgZ0M5im4u-_Ek2SUuzg=w2400"/>
               </CardBlock>
             </Card>
           </a>
@@ -52,7 +52,7 @@ class Cards extends Component {
                 Chapter 14
               </CardHeader>
               <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/4FaTAdTEZx3D2UiXfB64S_qsPZxtqRC7PjQRxBKspTB05n-kqQB-uA2Lh26rTmZN2pZYCj4j3gsFoWQtTztGFn6drrA2i2BQ5tO7Rms8AjjnlpSBCHrWqq858wQCoOQk4wHvv8_nDzHyGcmFYJQ7j4ye1Thzeh-AvylXtE2bmPH04fgzEBIvHDMeQ9oNe4UOcd8gUU-rMRrRj6VwfqY6iA4SofpCXPLx9pJQMAiEg_7PgXASFRNm1QL-6zGlemPOzbiyCSQlxlkNcZVEAoFJBwEjmjgwdJnylFB9TJgZdZ6sEjR8Iw4GB-XDDQWPRD-1UGS5_SgrgRRb0HmSGd-9PS_hjSckavQd2X-hXSKP3BVhaRKD6sDUXTgbnor1K-TKEdW8QqauzJ-VZUMqxcn-ravif9ISi59YjDabLeunT5mvyWoqdlmWiHEdfUx1IVpwrsHLKVCx2_jYsoeGo_m3Kk-JlVqJtfunmUQ8ifZnV-7g74E2IZTroNV8C428zHtQVwphVJqM7L4ogOJqYKIpkw--jRLMCq2a78862sOIn3xHt1VdI4c8lRFolxQ5gdQ2ZNOSBnWqL5oRw5wBPdap2nnNFSEQQE5CYNmDSLk=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/Iwc_oDu1q3Ty9wzxU37J7IVlg2YZBr4WvtypSJXtGhLClE3EmtyadOyTt50W1_oXE1cfVzj2MYY16ALwqvX1eD5e8Q16QcvgrZTrwzkrVqJDinANjnR2ajL-gYi6kXw5Ymwct2-YKg=w2400"/>
               </CardBlock>
             </Card>
           </a>
@@ -64,7 +64,7 @@ class Cards extends Component {
                 Chapter 15
               </CardHeader>
              <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/pUTJewDqksdSS10wi3tSaOYLqPVpxYZ6LSyvhbbgQg14KoNeiknmPgv-6dWb811-j-PEMWYrr3r6Tpwo9pHo7T0Pxnq9eMp3BhXNpUYHHa-NcC4KIpWguJfSF2v4rSVwa-b2RO-p4GF6MeKYiplp2F5zNPvY242NMwZQVYB2dUj4r0WQoFR3WooYGs44DA8SUz_dO2VvXS4CugrwRDSEeKximEGxgFNi7E3xt4aaevZ7Go3kySN-dia1VKpzuKlo1_qBRVkoJZixCHsce8M8M8viJMxBJ1l3AsdvcVgo9Ast5VQENtf1W_un13mOafQEguoL-D7s4744qgiYJi98MJfvlUUcU5ZYTGEXiq0gWDTgY8WMiieiuDGAQhnXhrUKVkXSxE3vww5nSs8kTYRf0mTGUbgkELn1WI7ThOFJDTz2e3Lyzbl2llQKPdam8LXvoYCQMT8Zotj7u1c5ZD2I3RXuGRa6GoRqV7Urg8wuFZh9kfZ03NKP-6pZMd5fIRSsNJSLn5fzosdqWDqkKiMYrHt5JkIDDKL4VurTjD4xCe1fpCtYI6edCfD-PKwMsaHBf_SiknAU91VFgdh4AYBgybUPdj0j0Ll_c8e0vyQ=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/0caTf5GCoGDz6qwzOapIkfcFtCnoLUPT9pQLm-94riPOakklitay9Y-sFYDhALKx3XYBu7tgbePCNlBDPa3xRvFDW-RnzTb4lfQHQhaEutoNyYmyjGNM7MkUXumZlaekIPDnKq41Vg=w2400"/>
               </CardBlock>
             </Card>
           </a>
