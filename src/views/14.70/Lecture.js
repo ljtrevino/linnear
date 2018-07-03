e7dfc027012222bedc1cd0baced4737db062a76d
lrylander <linnearylander@gmail.com>
2018-07-02 21:51:10 -0500

    14.70 images

--- src/views/14.70/Lecture.js
+++ src/views/14.70/Lecture.js
@@ -13,7 +13,7 @@ class Cards extends Component {
                 Lecture 1
               </CardHeader>
               <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/TLSuIlU5YLz2CRq8F4pTA4AXgT3EKMrcV101rjbPyMElhFesfbXRY7ahm86l_MO8x2p4AL3L7PJqfOpF5suOhsa6-wmomTVYr8uK6lylIaj5APPTbuwwWZNU53nsf_rKiWa-qAaEu3m3fyhyPJqOtTmM94no-yIlYaM36D8ZYhWqfjl2D6vbLCU8pRbFH21AxJvxzVZWKwSmKHoLvFB9z5VVCdwBprXNDxO6R3K1fuftiOW3C7qQKj6x6MBwBTm5j711oHAvwHwK0sbus2RstBqmFpt1ido0BbCbUyEeHnXcfpp0DxvD0qJYb_XaFfuGq-kP7IFfvdE5_U_3prMEIM0YHVo9Pgi_vQ_PoJGU_iBxd9y1gzxem0-hXt1ueGWQqjog9lKl-TUpqAAkM8DVNvypVZ4JVNIZQrDrcVRz_-exjUXPsg9fzBd47K6VljYaiScwpe9EVaAuT83C-GEuCBarkCmFZD7Xscupz2fboGFOnBfT85YUlVt2qznqVVP8Gxr__mmipN1wMd7GSHCptCgE2p11wlyL2WGXH_35BrM9P2JDzsfKtZch5iNuqtXJE7HVExw-NWLuUPPgfcNOfEeQqSH1f91BjrbeETg=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/AJ7VX4LhFrzoMI6GEo4BvdMyFJUv0QmgO0k1WLfTQ7OzzN_dUXD9Vk-0Do4YXWiZiR2z-8071Pqb1jYrf9lFmSz_3YcXuc1XeNb1zrer5q1JyV-LwgPhPRFtL8B30KuZxGFu2YnJNg=w2400"/>
               </CardBlock>
             </Card>
           </a>
@@ -25,7 +25,7 @@ class Cards extends Component {
                 Lecture 2
               </CardHeader>
              <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/SFklnKYrE40AVKmfaeYO5UFOLpG-utWINx7n3pVWT6eLbbnc-oX2xLimH5Vqp4aLJLQ3Hg-HnFXXiF-6covo2pCRpsNqRPKzgYNH-4WAdLsgRmCXzcTn_Oe16lsVGQZjyIb9zRlqLiDH6og9FiSQ4dI-07IwBs0W9kkHeY2dHyw9qRBqRXGCEyWCPxVT13I_5p70FTLo98TH1OF6fRtlXSo6zqaju9-Z9qQHXzdrymxdHGfF2k7XAlY6dBRBtztLNNW9gmm4ot4Yo1lGpYjmpH3NfSmow7N_tbYsJCOLQeN96HOj8H6v1c8HcKSCXtUPZO9OzPjjCwPvV4JSR7rfAuAgril3v_H14nmxJPUMlNMWsPmCrNcKZUz9MRrVGtrXx1f8zlssvQNpNx3NlSSUWlPtQ4Qb_IUrUTqktxcUNiqCIg3MMuuc2yME-IJgNTllAN-e6CzSvmfBchMQTKNvZQV1MK1fFCSzc9dl7Gjeu7WxO-di8HVYpy2N1h0KAgosqAsW0QuHnx3LY2z_-s_PXrcfgW7ok06ZgDSVM45Hpgptpy_KDk9-kMaMRorHSeAVSzwurvvPXjHU_Wfz-kcp7uFBVRgnSCRCBUqvsA8=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/3IcMS3dzrmG3r3b2IW6rLMyD-CS_z_x2mTAQ_j007_UBylH21vL-oWaistIZaU14jNW907FhMU3gX7Radvw8Sgwc9ewSbPji8QMu5poZzyNkDJ7lB94k8z5f3BhtO1X-8ad6DRi1Iw=w2400"/>
               </CardBlock>
             </Card>
           </a>
@@ -37,7 +37,7 @@ class Cards extends Component {
                 Lecture 3
               </CardHeader>
               <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/09bqUG833m1SLqQZhwlj1PEpdKFK-elXTs_lhLgv-lKq02MnsU3795Ptf9q_ZduSpUgUOeS6iED3P7-145W489EP1RhjSSf01lTCqmbAwRghSA0FUO_LtyI4Paa8R4ZKnj4uZCR-IbnkltmzoyoBA728uWG9DYPwQbtxXTRjmzPyCRD64Kki8Y7H0sD1_5qCAyYPhznjkboZa9rAvtwTimlYLdObYx8bpaAkOyIXNJ2w_9NzMaqzKXX3zvCpmeihg9--LalXNwBsBtls7Th60-oq05ib4cCY-GdunkqRjqYB9VnEwKGsQEza51lmiSEWqZP1lxePsnkeUoELyN5B66UhO5H9F5eKIU_oUgP_2aHKHRoPjV-NRabQTP7Htem_9V8t1XIuZEOePEwg1d7bwGlPAQEpNcxC7m2eYkRJVMWtxn1zLT1vjsBgSmpJZcDQ9YBP-Mvw8i_N8GeU9a-Y2vOIc7PiXU_3pZbh7OsffdEmX55pF1J-ZmLVGv6urwkk35nxsH5eF3RBEl9IK7uJDblqx3uq-MHJaYLyt95gXKnoBz5SEP-QYBlRkFJXCW8lc0bgkjOEwjjAEEO29j422mz-E7U8m0ZMnRpQd8Q=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/h4yJhMWbUhQeymm_pWlUBP_yBZC8NA_lHiieDgyF2EYu73zyrXkTAm1CZT7YZeInhi2-OEnL3BFrsRAv8KIOgILBIRXNust2izld_u8HZctiQYajCjPRg8wMYaMwwBbI9zpJadE_9g=w2400"/>
               </CardBlock>
             </Card>
           </a>
@@ -52,7 +52,7 @@ class Cards extends Component {
                 Lecture 4
               </CardHeader>
               <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/2F6_g5yM2QaPxn4UQjqZt3n5EO5EmtbiClW-52MI5hXP77jrRO92shumeVvS8O6eVNu-xWy2BxRxRvrPleCoovZ6GqusAXc2V20Gm_FXCnqhjzzmGM0ALAG1F2YWCe7XIdPwptLUEN0V4SjRhDttsTuvkoCGg-XaO82MGnByRZuX3u4BgTHwkuDNm8pApWnqA5YftMhec9euBhoGLtpdz6gPwxackOInxyOlXPy8lMpF82Kb7zHb7SQuQ20grp8FdnIXYTIbmbvKEWm_bRx5ZbUPsqQLfFYuMPuoRdKaKNEcNnt_kW8k_1qfZ7JsUcfV3D68H-yIxAGxgy3nxx3xrJsU_vxZNeLTj6zPkdd6KLAaPQR0mlKIriPStL-Xk8ZbTlHC8C4oiaAcTD4skEJATVs4t4FzEkehqhbUieVQdcagmCTDaR8yWFYV9P9uEuMYxqx4m-qLEyZIXzkVQT4qJ6HxSQhQ9HfLxQu7w8_vvAcTsTT4JufaFL-Y9I7oBS95HstfnRRtwOgfkksT9c2w9DfGrEZxywSmcFPPZSdO-uDxj2oZqo7bNtP-fyZoJQKg1lDKO6C7-o2EMhCRzGF2NipPQ1O99mzhpmJ3lfE=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/Xmaqg6-OyR48SfssFhXsHwiy4yKE-xFr2UXmZDv1FouR2ksTsoUi-vVWm694ErOR9hcy3WPCsY0A4PosRQpqzqMBD7KdK17MWNYrmB2uRfmLVZpbQ861Vmqu2QiBCZxDLvK04YqNCg=w2400"/>
               </CardBlock>
             </Card>
           </a>
@@ -64,7 +64,7 @@ class Cards extends Component {
                 Lecture 5
               </CardHeader>
              <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/yN_LuiiSZzXpLd75M50crAquJed-ycJJDecovDnKTRYnyxknja33kWHDepiOlmU5QlmNuYayCweiwKL5LKlFIfpgV1-1AnUY1NFj75LGI_fn7yBK-PMQ0byBSUa2F4153Aa5846Xk0aIoZrB_y3hY7I_farappry76Ax0nJbpERWyfq8Nvn25PamPGadC617qPce-juynzgPfHISCDLmWol0wY3xaOIxQ7egFKZPUK7NNEkr1vLDtlf9qxyVwKvWdhIhSyBwDpeg7R8Q7F0Rl9_hZAqrVsl4yux2oyv_8ZHryHN-f78ZV92uq635G3IKFDLJv7nywgLCGI1nfmnAcWFkK6c7iEFHGukm_4p3hMCigfgIYpdU9lkMu6yQPsJJQQqy2roJMcuBSY4mhnNx-JvOosDe5sP55xmcnuacbXo7OWuFz74zmZdPLMwk331Rwv3vWQwrcgwpekDHJUo44_WBcNoxpFEeljA76rc225DOBbF5HDZv2wnn8hAai-kgd_tgXWXAHSAOVg3Ic3CvIBW_DbvfEeNJazaGeMQKuAEbwv55DWZxtAPzpkTwBtCVxHTUiz4jq9uIJyM0Irt_GIzATSUo68aCjj6aqO8=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/cTtTq3y0nS-jnMOuXqoWqhA0csZNPby3qvsS9PkDCvvAKbRqcb6V1V1Gn6o_BAR8e2bAyyidaVqhYb0Uoin8BhSUezdHZHVxH219MNRlT0moLgHPukTS8vFNJGSRkTASfstpQaEkog=w2400"/>
               </CardBlock>
             </Card>
           </a>
@@ -76,7 +76,7 @@ class Cards extends Component {
                 Lecture 6
               </CardHeader>
               <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/5zZNFUCiLRzAED_8svL6SzlU9ul6lPGj7P8LwfwhiI7Mm595UepqRhytWfe_x9ogsuXAyc0IPht-_YLEaznGBKMzHleaOYgQPVeNaZr_gZcCYVClH_Rs_uyBOPTbnyS9JfyyxMNEFoix7o076AdSNQRrWPd67ehavsOOKksvsTA_NUpLqxO0I5Gz9VpO9gioNL5pi7NRK9qFWjYBN-cLEOhQpThVL6LST6lHWJd31dgV0BsxcwDu4rgAjUE37-FK1SQMQoKGUFpE1tql6OyknsmdI47x-k5TydfnkEOuwpmHRGLqBpMR6wHtJhNWM2rLdjeehaQaJKOmTckYTnAF7o6qXiy3mwYL9QgzAFpVzAfPL9HHSbZLqQFQSD4FIgC9samwEVSYuRG9_ydYFS5J1vAT94fzXfoioYt6gxcg6IPVZdlIF2gitPBBH4j-RM2l7Rpk10NQUZcB0MarnmRvjXtNAy92GW4JnKGs_GbTaz06rteuCP-asc69IMmOWqAZ3yr-R_lKYAlWOU1IM5h_037OP7HYrAOQ7eQiVleioh-sH8zi0y8rExUYHu4cJ9mPj4HYTd5GcVJP2zDmykhL79FbtsZXD3KiUY_LKB8=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/2HGHzoEphKZR6APitVETcIJ9L3vXKWEIsqSLYgKoSScEGZqpgxeKzM0pFQTPIa_txh6Q6ygIYjiacMN6YZQkVo7FFCe_KYZjuJHgc44du7DP2dSImetn1fsaU9qGY6EszZGWK6GUlg=w2400"/>
               </CardBlock>
             </Card>
           </a>
@@ -91,7 +91,7 @@ class Cards extends Component {
                 Lecture 7
               </CardHeader>
               <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/jMRIFQPS0RbTlkKiX7OmQTMOnirFZB6PPrb5lkJH8kgi6ymk-bQ-EeEHPv8CAkL18qoIIhTnt4mtR3ktmQWRp50OzUkGUPDD9VqGj8dOOX8FT9g0PCgselaKhK7bnXOOf4hTY1wSIcOzk8FRTtUxhNwRpcZqOL_ceM3mbgS4H4UdPwVz1hS1XvxGGCkqsIumD4Pqbw3wPzwDK-tLVkZJ8ADDg0-hKPT6h8WiHFhAQno34tGddBNS2qZaxvYQJuceneryeQNxbAlxCXWVIdIp0ODbnaiyqOr2mWVGNOcQw1muu7Ues6wCnamXexrBMOqLEo-ZkoL7jXATdKfsDEDUbluMZHNlmw4RBjt4nmFabgbFsGDU9CnMt-k82uH5ukALPFyL3MZuhOK09BVQdKvTknDLxV-ch1ISlr6mxGNbSYqnhyR1PSYg_t4xg6VQe4S5kC58MICM3sN_Dyoo0C6aHfC3vWn0vH7GM5xGI50eqzhZ6A7OTK7oocUKLloI7sQBjKerxAJzdlzyPyvFdctUNAhagC-c8Wm_jCM8LgZhZQPuzlJUgudpM4fy3WGZ987xUo7NTZcnSq9r9CUW4Gf3MV8U7J3T7mIHKTMWn_8=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/_G5-rYxmJsC06UAyBWHu4Xk1R6KNMAvPw-4YXGJ0VHlesvy6Dt_a3cJEoEtWQPhpthlzgX3pjWuBZK-NJytbOYw7pgzn_aJxIbtSQwj-Mm_S2is0XblYIlIu_mdgvXwYYoK89hPKlw=w2400"/>
               </CardBlock>
             </Card>
           </a>
@@ -103,7 +103,7 @@ class Cards extends Component {
                 Lecture 8
               </CardHeader>
              <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/fe7v_2Hc6GPOCpCLus0-aUOnR5eYAn0yAnyYo1lPAoaOAhArs68NiHs5gCCmdowELUNui-V01m32ieMBn6P64RIUX9qQgG6VuIpQHjNcQyqrlOhajkQl1h1H2kl9fE37ICpI7UVc67XpLiD7TkEaOgeZmPrjbvDDb8SDglzFVqRZp85UX2-GfV7Dogfft_BoLu63u3XBqaACP2xqUN9rUH2MtRzTHoDSJtlDSXuLbqKSUIKPS-xbmqgysuYQqaQbFfnYdrEWtAl7iO0QIclqVuhjr3QMQI24DnED865YZu5sXLfHSpRpg58C_pOqTJ52tZE7ZvKTjhXoiXHreVLXtZDsOfFDuzeLbn0qHh5ptwGSMF-FHPPbPVUGiIo6edrIiP6EM7FGdXGNAUv07Vrt9gb7IVzsrlvK-wB2lzLEnb5mn5wcStunNFfxP1govpQ10O-VufITeDCdZg-K8NfZcdkeen4longpTV3IBUmNsQ4V1feXaZhODQjXFWeJQZJaYGxRDrXK2DepeWI1TiOQ8GjJdZYQPvbuem8gt9UH1sIrlAN8U1oAxZD88-m_Zc5a_MaG1ImIRoTHC9eCCl8doaXg26M8RdQ3KA1963w=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/UoVCOAyHZ1afK_JgHcqQlm4T2dM-i24-_F1-zcmXQcHzbr0AIGBWwurfHTXqL2Y2qy-ebQ8JzzlBNax6eXeyrggDvLoN7gKmyp1dPsgaBcLXeZ37eseSLnv7fwW7Z8EXk5G2A6BdlA=w2400"/>
               </CardBlock>
             </Card>
           </a>
@@ -115,7 +115,7 @@ class Cards extends Component {
                 Lecture 9
               </CardHeader>
               <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/Aq1yJ7niZQcuvRfstC_6ldaXthrYYt_KOJ_tHCcWfb2oTiRTD8EIdt0_OpIKuMIeGbvX-g_PJ0cXF-uAIPnGgbObMEcLU4y6Uu3Hh_TtirODBHL-TRxGIAAAM_pN4iAc2NAC_IwkWs8-P-ZuO5ftzJaNRDgQ0B8xhP1zq0rVkigIYyRk4-YXhZFO37yQv8SRhJY2LVCi2pL2M9o9jM5_GL5c1UYh998-mRGfoTNhPC-dqqQTkGz2nyw56KiQDIm41rMTZe_1mNshx4MdqnMrD8XB3mlLN9AfP_a54SiYFyEJgtYm6vRtWkLjpIB33Xi92SfBD6lhQOD1qjucmBCKI68-fcQprdtYNkzIdtWYYVMmbLhnm5uvkrAwJvOYR-noRXhBdwndQpbxpNYxciMItz4dYpTG6cJ8ddf0POkdDIKq_j8noftjB3HAttwCOjaQ3-dMaDjxanmrPuRBOvabtfchg9CjYKet56_XF0qfhug9icSMf_FbEgBsbBgJVcdMzYq9u5soXFPIWiRKMlxVBNVtCi8oRiS3HEs2Q3r9kSYVsK1XL33XeuJqOPudzF1uXDZXUbycS2SSRuHNvLzS4Hw2KA73IaXRu4BPy84=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/DaWjN5iAEam-Po2JwCHUWyUF1-441lIorv3b3Q45hRLMNuOfK20E2svUmnTJIE6UPq-C1_zxxNFVxm402gvSFikOwAKrfF7HjZBbAlhn8YZUgc7KOd3dt0qFEI6HVO9GsTwrB7qw7Q=w2400"/>
               </CardBlock>
             </Card>
           </a>
@@ -130,7 +130,7 @@ class Cards extends Component {
                 Lecture 10
               </CardHeader>
               <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/wKWT7v91qg8U6jeDcuShc1AyYFLmkusMOO_A15LkSo7cUq3sRWx1qg9rEEjJ9p_4WFLqv4mlWhU9-AFDV-Cb_5gKRtw3dTDpC8SYgNYB4GQYF6SRXOOyx-VUEezwA5eTdV6Bx1WDhsSEGu3ZoN_7NIh5ddobvniosaWf4C6ny6moWd4xCJ-eurQxKiuQmeh3NJkyjJOptY6UegfqH0D1oAxPIuFgjamfnFz0CN8xpFABILnElP04tLBO4KRCZBxLvc7bYYAGOJAMyvVhy0sXsMSyBZ9LXfJ4VuooWn34MAWz93iWB9GyFOE59ufyEEWC6boF6X7xJY1FmtDJj9DhGMnMHGAuEv0nMSF1zYv-RT8mkrgQrDsgGAgg_6b8Riv_r7UGSmSakn-Gz6owcyygcNKrGarH7Sqfh87FV4dN6R4X8bgwvwfSlRgDu5ABY7hxNe8-LzJYmJ-ypdLqZI-vUd3Q7gTIR_6Lcs23jsbz44TmbaPmA5GETNq1C8mgD2oGQ6do6u0BoeFMVVwLLnCtI1I6LRJbTM979U3Fa94jMAmDAd3a7WaEOdfHLQ__OQSwJHAnGm_2e4z2Rcsn7Kzd424Idcz8nmOcBH3hhD4=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/RJVDUSh1_gIA6KmY7P4IN7faR1x_Ox0f8MZPSgefmDOwxD86utdhkkd1ivwUSjMyxS8UeQb8QvmgihfI88QbRYToqIGQA3N0d9UicZSC085aTfWGVrTzDc-xYcKIXSBm1ueE2Ji14Q=w2400"/>
               </CardBlock>
             </Card>
           </a>
@@ -143,7 +143,7 @@ class Cards extends Component {
                 Lecture 11
               </CardHeader>
               <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/rowocq0d3DYDAxibgzjwlS8Fn76WFfadXqRgGNSb9i2FUgkVB8-U8si8ZuuAqBF_-cOmAVOGanFC0QqeblbB-VBMELhYCrdIN28c2mLq6JmCld1c7AuRSRuSoG6RcEGSJHMoqogDuUSMUlIwlgaqWVMzxBeDb1grHYdWZSbXcAFjMomJFZfhcnc1TG5687X6OCRaJhmpWY1ab4dMiENP31ffbCIzlDTlcfhoJip9PN0NnNsaruphJN2JtsknbGO-rYnDoGiYRY66kMrTDhFcIj1NuAqzIcZyJTqvl5Kjw7Bn0hftzGDxXvXkpBMY3jUQJ-q2_OwhKyLok3VK_lzoE5IebglarJRBlZgaFZdW9s2eBYOq2utNwPcRW1km0Qn5ELlQDeEN58exR_8RrQIAXrEThCIH7cN_2kjwkJVrLAKOikZVHpLN02Rj07oOWXhToUrUa-tZSJhSt8_zUe8ePWiLQXgTYwoY9hLaoR98OJ3EYJcqfi7eaTfsTbJ3XXwhICpBFWPf0Lgf1F-xYkd1uB9O-dqb4q-U3R9_b3p2KU7iXoizUz6cHxV6m90I2lPI08Vv5yicL_FGbZGuMIIHqpPNm7APOwcba2Z0x0c=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/dmelQ4z_onEsUb4nHSnHCoOCbIHqpn53t0F6OhUbu9GrcN7gPhCTQz3X_zbOhjUqOTe-pydmnojTbRvmOe_bB_Kh57Le0KeIHVRiqv1_X8gWcGrm4lBZUBDBgbwAPWVfh-sxkDv1KQ=w2400"/>
               </CardBlock>
             </Card>
           </a>
@@ -156,7 +156,7 @@ class Cards extends Component {
                 Lecture 12
               </CardHeader>
               <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/X9HQSF6ZrjSTpP9p7NTVEMQAT4On03EZax-IIMlebtwjBlbFtQ_u5462fcxi-TmtlMgf1-qJgpBp2Ike7DGjOTVRNEFATAuzRwNRVkRT0X4XtSeNXL88Iiu_BXIFcC9Xy852fNbAaJp2yH1BgB1HxiyMmQnqZ2WZ94bNHyjZWCF-zJtGx_fzhf9fB-NVMbwrIZRO_5OgjCym51GtlLWfeF1BRaz1D1BneEM_ofS7C1yHtBM4XcOGP3NA3NsIqEBboyG83cyOndxqLY1bdwpbLnF_TvelhEZGpY-6xiBiAqtyVCnTFEYca2UF97qVFCmB9YSWP--f9Sy8szCY1PvI-3wtpqJ4gX1f2Hn_kALmo0R52jTGd261oqwtP6pUTiYaVpzsyQOkDu_GzK9g_BEUkw798g_2g_s_O2_kov9Skk_USjWsTpRnjwNmO9jBmFTVU88YVbR0XGJ_d84mA723sawHvw489PXlS8WPyKCettcBI_k5DRK6N1vLn2EnT3w2Feu3QxLzrIev6rE0pJTyHn48ikci0E6gEmGGGodiSYcF7x7qHT6gIIDDmzK73GNC0PdUbv11wHbcPNaw-G98RryU22Cqsv3nV2k1liM=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/NnTijQtdj5bJ1eEys60r31UPzewSWbUe3ACTLJl-KFzBfKez2xV7z1-boVhmjHATDtdyRqWBVCS4ylzYzHiLvTD0T8bV6jcfpa8-6weFnBOKFeX6CANRh0i8kpnu9xnBRXHcK-Xysg=w2400"/>
               </CardBlock>
             </Card>
           </a>
@@ -171,7 +171,7 @@ class Cards extends Component {
                 Lecture 13
               </CardHeader>
               <CardBlock className="card-body">
-                <img width="100%" src="https://lh3.googleusercontent.com/anDa3CQjMLi5My3xfXLe47hxb8BYh7s99jp3x1j8dSlK8rH5HzWlnUYpU9Hi3DjTBpuGoxxCqVviUf9GbamCvhZ8_nL0R9TL4cf3ObJK4MupQa6JrCHtoAecopwwTJeJkwvRWflmcQ7wrY5S3nshALEDMmeAKkPHfKx9VZziH2skjdDFFmP9jf0X-4jItJxcDckLuj6Ntbsg7qYddzpYz4KuXDCsTaV0dqefRTR8c0hgF69ledJ3SbGGaZSuz70bA5s0vyJFnUsPr_kXX97LrWLPIGkPnXYVZAff4iHQpUDIZXDxjGZ8sbmp0OHrGNmmUy__PR1v8y3UKbPxmJ64UiLTZKTRHRK1ll7F6LBX--3licvCcECrAARkbIEabpqbCfqHtzUbcWYzIRfnLvyKlVCiLR9BO-OKyJ7_V6UgJLtVPQ8-IYWstD74lgMOqdqKzFtyhkQ__5EC81Fb3I23TCPoC0OJ0XevVNVEDr1-YJ58QBxKG25-d4JZgj7CBMzKuketW23u7HK8utvkTwPd0av4FfjxbJUmlnArzyI4scczKmZJjmD0XAryZmK9h6slctiKtXop_Vrav-4PhcBoFRxDx7qR7flWXcX71vQ=w1315-h1700-no"/>
+                <img width="100%" src="https://lh3.googleusercontent.com/RJsC3n7TMJB-UzmcyYSmzec7yiMf-1zR16pQJgW7t0MHavYouEruBJn2B7MydssRF4xOuGSxpPM4QR3W4B2mI_mv7kuMabKVWLUZKM0mfSUzljUaau-lbD253UT3MQRyki0GtvSK-g=w2400"/>
               </CardBlock>
             </Card>
           </a>
