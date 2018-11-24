da03c0823fa9b9e6498c218ba79d5863c82c7fb1
lrylander <linnearylander@gmail.com>
2018-11-24 10:34:33 -0600

    Footer responsiveness

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,8 @@ class Footer extends Component {
   render() {
     return (
       <footer className="app-footer">
-         <a href="https://linnear.info">linnear.info</a>  &copy; 2018 Linnea Rylander &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Looking for the old version of logic-l.com?  You can find it here: <a href="https://lrylander.github.io/logic/">lrylander.github.io/logic</a>  
+         {/* &copy; 2018 Linnea Rylander &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; */}
+         You can get to this new site directly using the link: <a href="https://linnear.info">linnear.info</a> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  Looking for the old version of logic-l.com?  You can find it here: <a href="https://lrylander.github.io/logic/">https://lrylander.github.io/logic</a>  
       </footer>
     )
   }
