ae09d1fae18a28a0808c966892c8084eec127b01
lrylander <linnearylander@gmail.com>
2018-11-24 10:38:59 -0600

    minor fix of link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
     return (
       <footer className="app-footer">
          {/* &copy; 2018 Linnea Rylander &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; */}
-         You can get to this new site directly using the link: <a href="https://linnear.info">linnear.info</a> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  Looking for the old version of logic-l.com?  You can find it here: <a href="https://lrylander.github.io/logic/">https://lrylander.github.io/logic</a>  
+         You can get to this new site directly using the link: <a href="https://linnear.info">linnear.info</a> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  Looking for the old version of logic-l.com?  You can find it here: <a href="https://lrylander.github.io/logic/">lrylander.github.io/logic</a>  
       </footer>
     )
   }
